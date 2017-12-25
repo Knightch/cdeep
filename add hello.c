5fb59f69b7f6b502b220f0bd74ad25c8c2c8cdfb
Knightch <33832428+Knightch@users.noreply.github.com>
2017-12-26 00:18:52 +0530

    Update and rename hello.c to add hello.c

--- add hello.c
+++ add hello.c
@@ -2,9 +2,11 @@
 #include<stdlib.h>
 void main()
 {
-int n;
+int n,a,b;
 
-printf("hello guru bhaiya");
+printf("enter two number ");
+  scanf("%d%d",&a,&b);
+  printf("sum of two numbers %d and %d is %d",a,b,a+b);
 printf("\nfor exiting this programe press 1");
 scanf("%d",&n);
 
